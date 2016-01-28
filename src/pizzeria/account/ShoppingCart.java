290597ea971e593300bb653373699cf8009984c4
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-01-28 18:43:28 +0200

    Added shopping cart

---
+++ src/pizzeria/account/ShoppingCart.java
@@ -0,0 +1,11 @@
+package pizzeria.account;
+
+import java.util.ArrayList;
+
+import pizzeria.menu.IProduct;
+
+public class ShoppingCart {
+	private User owner;
+	private ArrayList<IProduct> products;
+	
+}
---
+++ src/pizzeria/menu/IProduct.java
@@ -0,0 +1,5 @@
+package pizzeria.menu;
+
+public interface IProduct {
+
+}
