f3c0b51a401a72edbc60a5fead41bfb0cc1b5a25
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-02-09 01:45:56 +0200

    Added shopping cart. Not very stylish... i'm tired :(

--- front-end/js/custom.js
+++ front-end/js/custom.js
@@ -2,4 +2,8 @@ jQuery(function($) {
 	$(window).scroll(function(){
 		$("#login").stop().animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );
 	});
+	
+	$(window).scroll(function(){
+		$("#shopping_cart").stop().animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );
+	});
 });
