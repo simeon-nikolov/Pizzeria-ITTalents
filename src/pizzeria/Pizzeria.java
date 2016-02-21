79f81a72820bceb2b6ad69738431e2078b52f895
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-02-21 19:15:21 +0200

    removed some collections from OOP

--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -1,9 +1,6 @@
 package pizzeria;
 
-import java.util.ArrayList;
-
 import pizzeria.account.Account;
-import pizzeria.account.Administrator;
 import pizzeria.account.Order;
 import pizzeria.account.User;
 import pizzeria.menu.Menu;
@@ -11,43 +8,24 @@ import exceptions.InvalidArgumentValueException;
 
 public class Pizzeria {
 	private Menu menu;
-	private ArrayList<Account> accounts;
-	private ArrayList<Order> allOrders;
-	private ArrayList<Shop> shops;
 	
 	public Pizzeria() {
 		this.menu = Menu.getInstance();
-		this.accounts = new ArrayList<Account>();
-		this.allOrders = new ArrayList<Order>();
-		this.shops = new ArrayList<Shop>();
-		Administrator admin = null;
-		
-		try {
-			admin = new Administrator("admin", "password", "");
-		} catch (InvalidArgumentValueException e) {
-			e.printStackTrace();
-		}
-		
-		this.accounts.add(admin);
+
 	}
 	
 	public void register(User user) {
 		if (user != null) {
-			this.accounts.add(user);
+			
 		}
 	}
 	
-	public Account login(String username, String password) throws InvalidArgumentValueException {
+	public Account login(String username, String password) 
+			throws InvalidArgumentValueException {
 		Account account = null;
 		
 		if (username != null) {
-			for (Account acc : this.accounts) {
-				if (acc.getUsername().equals(username)) {
-					account = acc;
-					account.login(password);
-					break;
-				}
-			}
+			
 		}
 		
 		return account;
@@ -55,20 +33,20 @@ public class Pizzeria {
 	
 	public void addShop(Shop shop) {
 		if (shop != null) {
-			this.shops.add(shop);
+			
 		}
 	}
 	
 	public void removeShop(Shop shop) {
 		if (shop != null) {
-			this.shops.remove(shop);
+			
 		}
 	}
 	
 	public void makeOrder(User user, Shop shop) {
 		if (user != null && shop != null) {
 			Order order = user.makeOrder(shop);
-			this.allOrders.add(order);
+			
 		}
 	}
 	
