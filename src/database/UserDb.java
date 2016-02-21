6ebb67ef822496f642cb9c8df7cc8201ff617d4e
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-02-21 23:36:29 +0200

    editUser works

--- src/database/UserDb.java
+++ src/database/UserDb.java
@@ -57,4 +57,43 @@ public class UserDb {
 			e.printStackTrace();
 		}
 	}
+	
+	public void editUser(int idUser, User user) {
+		String sqlSelectUserId = "SELECT `idAccount` FROM `pizzeria`.`user` WHERE `idUser` = ?;";
+		String sqlAccountUpdate = "UPDATE `pizzeria`.`account` SET `username`=?, `password`=?, `email`=? WHERE `idAccount`=?;";
+		String sqlUserUpdate = "UPDATE `pizzeria`.`user` SET `first_name`=?, `last_name`=?, `address`=?, `phone_number`=? WHERE `idUser`=?;";
+		
+		try {
+			PreparedStatement stmtAccId = conn.prepareStatement(sqlSelectUserId);
+			stmtAccId.setInt(1, idUser);
+			ResultSet rs = stmtAccId.executeQuery();
+			rs.next();
+			int idAccount = rs.getInt("idAccount");
+			
+			PreparedStatement stmtAcc = conn.prepareStatement(sqlAccountUpdate);
+			stmtAcc.setString(1, user.getUsername());
+			stmtAcc.setString(2, user.getPassword());
+			stmtAcc.setString(3, user.getEmail());
+			stmtAcc.setInt(4, idAccount);
+			stmtAcc.executeUpdate();
+			
+			PreparedStatement stmtUser = conn.prepareStatement(sqlUserUpdate);
+			stmtUser.setString(1, user.getFirstName());
+			stmtUser.setString(2, user.getLastName());
+			stmtUser.setString(3, user.getAddress());
+			stmtUser.setString(4, user.getPhoneNumber());
+			stmtUser.setInt(5, idUser);
+			stmtUser.executeUpdate();
+			conn.commit();
+			System.out.println("Success!");
+		} catch (SQLException e) {	
+			try {
+				conn.rollback();
+				System.out.println("Transaction ROLLBACK");
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
 }
